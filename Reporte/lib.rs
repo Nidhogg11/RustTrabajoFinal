0ba0813f0d154e6e3e151fc9d3f70d791e605eb8
Mariano Bonacina <games-mariano@hotmail.es>
2024-07-05 15:57:42 -0300

    Fix Empate

--- Reporte/lib.rs
+++ Reporte/lib.rs
@@ -20,8 +20,16 @@ mod Reporte {
             Self { trabajo_final }
         }
         
-        // ➢ Reporte de Registro de Votantes: Detalla los votantes registrados y aprobados para
-        // una determinada elección.
+        /// Utilizado por todos los usuarios.
+        /// Obtiene la lista de votantes para una elección específica.
+        /// Parametors:
+        ///     id_eleccion: u64: ID de la elección.
+        /// Retorno:
+        ///     Result<Vec<(AccountId, String, String, String)>, String>: Vector con el ID de cada votante y su información detallada, o un mensaje de error.
+        /// Descripción:
+        /// La función recupera la lista de votantes de una elección dada por su ID (`id_eleccion`). Llama a una función privada
+        /// para obtener los datos y luego añade información detallada sobre cada votante. Retorna un vector de tuplas con 
+        /// el `AccountId`, nombre, dirección y otros detalles del votante, o un mensaje de error en caso de fallo.
         #[ink(message)]
         pub fn reporte_de_votantes_por_eleccion(&mut self, id_eleccion: u64) -> Result< Vec<(AccountId, String, String, String)>, String > {
             self.reporte_de_votantes_por_eleccion_privado(id_eleccion)
@@ -43,8 +51,15 @@ mod Reporte {
             )
         }
 
-        // ➢ Reporte de Participación: Indica la cantidad de votos emitidos y el porcentaje de
-        // participación, una vez cerrada la elección.
+        /// Obtiene la participación en una elección específica.
+        /// Parametros:
+        ///     id_eleccion: u64: ID de la elección.
+        /// Retorno:
+        ///     Result<(u32, u32), String>: Una tupla con la cantidad de votantes efectivos y el porcentaje de participación, o un mensaje de error.
+        /// Descripción:
+        /// La función recupera la participación en una elección indicada por `id_eleccion`. Llama a una función privada
+        /// para obtener los datos de los votantes. Calcula el número de votantes que participaron efectivamente y el 
+        /// porcentaje de participación. Devuelve estos valores en una tupla, o un mensaje de error si falla.
         #[ink(message)]
         pub fn reporte_de_participacion_por_eleccion(&mut self, id_eleccion: u64) -> Result<(u32, u32), String> {
             self.reporte_de_participacion_por_eleccion_privado(id_eleccion)
@@ -61,32 +76,52 @@ mod Reporte {
             Ok( (cantidad_votantes_voto_efectivo, porcentaje_participacion) )
         }
 
-        // ➢ Reporte de Resultado:: Muestra el número de votos recibidos por cada candidato y
-        // los resultados finales, una vez cerrada la elección. Este reporte deberá mostrar de
-        // manera descendente los votos, donde el primer candidato será el ganador de la
-        // elección.
+        /// Permite obtener un reporte los datos de un candidato en particular dentro de una elección específica.
+        /// Parámetros
+        ///    eleccion_id (u64): El ID de la elección de la cual se quiere obtener la información del candidato.
+        ///
+        /// Retorno
+        /// Result< ((AccountId, String, String, String, u32), Vec<(AccountId, String, String, String, u32)>), String>:
+        /// Los datos del ganador de la eleccion si no resulta en empate y un Vector ordenado con: ID de cada candidato, Nombre, Apellido, DNI y su total de votos,
+        /// o un mensaje de error
         #[ink(message)]
-        pub fn reporte_de_resultado_por_eleccion(&mut self, id_eleccion: u64) -> Result< ((AccountId, String, String, String, u32), Vec<(AccountId, String, String, String, u32)>), String> {
+        pub fn reporte_de_resultado_por_eleccion(&mut self, id_eleccion: u64) -> Result<(Option<(AccountId, String, String, String, u32)>, Vec<(AccountId, String, String, String, u32)>), String> {
             self.reporte_de_resultado_por_eleccion_privado(id_eleccion)
         }
-        pub fn reporte_de_resultado_por_eleccion_privado(&mut self, id_eleccion: u64) -> Result< ((AccountId, String, String, String, u32), Vec<(AccountId, String, String, String, u32)>), String> {
-            let mut datos_candidatos = match self.trabajo_final.obtener_candidatos_eleccion_por_id(id_eleccion) {
+        pub fn reporte_de_resultado_por_eleccion_privado(&mut self, id_eleccion: u64) -> Result<(Option<(AccountId, String, String, String, u32)>, Vec<(AccountId, String, String, String, u32)>), String> {
+            let mut datos_candidatos = match self.trabajo_final.obtener_candidatos_eleccion_por_id(id_eleccion)
+            {
                 Err(msg) => return Err(msg),
-                Ok(datos) => datos 
+                Ok(datos) => datos,
             };
-            
+
             // Ordenar datos_candidatos por la cantidad de votos (descendente)
             datos_candidatos.sort_by(|a, b| b.1.cmp(&a.1));
 
-             let candidatos: Vec<(ink::primitives::AccountId, String, String, String, u32)> = datos_candidatos
-             .iter()
-             .map( |datos_candidato| {
-                 let datos_usuario = self.trabajo_final.obtener_informacion_usuario(datos_candidato.0).unwrap_or_default();
-                 (datos_candidato.0.clone(), datos_usuario.0, datos_usuario.1, datos_usuario.2, datos_candidato.1)
-             })
-             .collect();
+            let candidatos: Vec<(ink::primitives::AccountId, String, String, String, u32)> =
+                datos_candidatos
+                    .iter()
+                    .map(|datos_candidato| {
+                        let datos_usuario = self
+                            .trabajo_final
+                            .obtener_informacion_usuario(datos_candidato.0)
+                            .unwrap_or_default();
+                        (
+                            datos_candidato.0.clone(),
+                            datos_usuario.0,
+                            datos_usuario.1,
+                            datos_usuario.2,
+                            datos_candidato.1,
+                        )
+                    })
+                    .collect();
 
-             Ok(( candidatos[0].clone(), candidatos ))
+            if candidatos.len() >= 2 {
+                if candidatos[0].4 == candidatos[1].4 {
+                    return Ok((None, candidatos));
+                }
+            }
+            Ok((Some(candidatos[0].clone()), candidatos))
         }
     }
 
@@ -105,13 +140,10 @@ mod Reporte {
     
         // Returns accounts that are pre-seeded in the test database.
         // We can use them as authors for transactions.
-        fn get_default_test_accounts(
-        ) -> DefaultAccounts<ink::env::DefaultEnvironment> {
+        fn get_default_test_accounts() -> DefaultAccounts<ink::env::DefaultEnvironment> {
             default_accounts::<ink::env::DefaultEnvironment>()
         }
 
-        // Sets caller returned by the next `Self::env().caller()` method call
-        // in the contract.
         fn set_caller(caller: AccountId) {
             ink::env::test::set_caller::<ink::env::DefaultEnvironment>(caller);
         }
@@ -124,50 +156,5 @@ mod Reporte {
 
             StructTrabajoFinal::new()
         }
-    
-        // #[test]
-        // fn constructor_works() {
-        //     let sistema_elecciones = StructTrabajoFinal::new();
-        //     assert_eq!(sistema_elecciones.\, 0);
-        // }
-
-        // #[test]
-        // fn test_es_generador_reportes() {
-        //     let mut sistema_elecciones = setup_sistema_elecciones_vacio();
-        //     let error = "No es el generador de reportes!";
-        //     assert_ne!(error.to_string(), "No es el generador de reportes!");
-        // }
-    
-        // #[test]
-        // fn test_no_es_generador_reportes() {
-        //     let error = "No es el generador de reportes!";
-        //     assert_eq!(error.to_string(), "No es el generador de reportes!");
-        // }
-
-        // #[test]
-        // fn test_contiene_usuario_pendiente() {
-        //     let mut eleccion = setup_eleccion();
-        //     let accounts = default_accounts::<DefaultEnvironment>();//uentas predeterminadas utilizadas para tests
-            
-        //     eleccion.usuarios_pendientes.push((accounts.alice, TIPO_DE_USUARIO::VOTANTE));
-            
-        //     assert!(eleccion.contiene_usuario_pendiente(accounts.alice));
-        //     assert!(!eleccion.contiene_usuario_pendiente(accounts.bob));
-        // }
-    
-        // #[test]
-        // fn test_existe_candidato() {
-        //     let mut eleccion = setup_eleccion();
-        //     let accounts = default_accounts::<DefaultEnvironment>();
-            
-        //     eleccion.candidatos.push(CandidatoConteo {
-        //         id: accounts.alice,
-        //         candidato_id: 1,
-        //         votos_totales: 0,
-        //     });
-    
-        //     assert!(eleccion.existe_candidato(1));
-        //     assert!(!eleccion.existe_candidato(2));
-        // }
     }
 }
